cb4a0efef1ac82def715ef29a1a1209486a152c1
René Fischer <rene@gaehn.org>
2024-02-11 20:23:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,24 +9,23 @@
 <body>
 
 <?php
-require_once 'functions.php'; // Funktionen einbinden
-
-echo "<h1>Trikot-Waschküche</h1>";
-echo "<h2>Die nächsten Spieltermine</h2>";
+include 'functions.php';
 
 // Wenn der Buchen-Button geklickt wurde
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    // Laden der vorhandenen Termine
-    $termine = loadTermine();
-    
-    // Den ausgewählten Spieler dem Termin zuordnen und in die CSV-Datei schreiben
     $selectedPlayer = $_POST['spieler'];
     $terminIndex = $_POST['termin_index'];
+
+    // Laden der vorhandenen Termine
+    $termine = loadTermine();
+
+    // Den ausgewählten Spieler dem Termin zuordnen und in die CSV-Datei schreiben
     $termine[$terminIndex][1] = $selectedPlayer;
     saveTermine($termine);
-    
+
     // Zähler für den ausgewählten Spieler erhöhen
-    savePlayer($selectedPlayer, getPlayerWashes($selectedPlayer) + 1);
+    $playerWashes = getPlayerWashes($selectedPlayer);
+    savePlayer($selectedPlayer, $playerWashes + 1);
 
     // Weiterleitung zur gleichen Seite, um die Tabelle neu zu rendern
     header("Location: {$_SERVER['PHP_SELF']}");
@@ -35,30 +34,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 
 // Wenn der "Termin freigeben" Button geklickt wurde
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['release'])) {
+    $terminIndex = $_POST['termin_index'];
+
     // Laden der vorhandenen Termine
     $termine = loadTermine();
-    
-    // Index des zu löschenden Termins
-    $terminIndex = $_POST['termin_index'];
-    
+
     // Löschen des Spielernamens für diesen Termin
     $releasedPlayer = $termine[$terminIndex][1];
     $termine[$terminIndex][1] = '';
-    
-    // Speichern der aktualisierten Termine
     saveTermine($termine);
-    
+
     // Zähler für den freigegebenen Spieler verringern
-    savePlayer($releasedPlayer, getPlayerWashes($releasedPlayer) - 1);
-    
+    $playerWashes = getPlayerWashes($releasedPlayer);
+    if ($playerWashes > 0) {
+        savePlayer($releasedPlayer, $playerWashes - 1);
+    }
+
     // Weiterleitung zur gleichen Seite, um die Tabelle neu zu rendern
     header("Location: {$_SERVER['PHP_SELF']}");
     exit;
 }
+?>
 
-echo "<table border='1'>";
-echo "<tr><th>Termin</th><th>Wer wäscht?</th><th>Termin freigeben</th></tr>";
+<h1>Trikot-Waschküche</h1>
+<h2>Die nächsten Spieltermine</h2>
 
+<table border="1">
+<tr>
+    <th>Termin</th>
+    <th>Wer wäscht?</th>
+    <th>Termin freigeben</th>
+</tr>
+
+<?php
 // Laden der vorhandenen Termine
 $termine = loadTermine();
 
@@ -68,62 +76,53 @@ if (!empty($termine)) {
     foreach ($termine as $index => $termin) {
         $terminName = $termin[0];
         $spielerName = $termin[1];
-        
+
         echo "<tr>";
         echo "<td>$terminName</td>";
         echo "<td>";
-        
+
         // Wenn kein Spielername vorhanden ist
         if (empty($spielerName)) {
+            echo "<form method='post'>";
+            echo "<select name='spieler'>";
+            echo "<option value=''>Bitte wählen</option>";
             // Laden aller Spieler und Sortieren alphabetisch
             $players = loadPlayers();
-            usort($players, function($a, $b) {
-                return strcmp($a[0], $b[0]); // Sortieren der Spieler alphabetisch nach Namen
-            });
-
-            echo "<form method='post'>";
-            echo "<select name='spieler' data-index='$index'>";
-            // Leeroption für das Dropdown hinzufügen
-            echo "<option value=''>Bitte wählen</option>";
-            // Dropdown-Feld mit alphabetisch sortierten Spielernamen ausgeben
             foreach ($players as $player) {
                 echo "<option value='{$player[0]}'>{$player[0]}</option>";
             }
             echo "</select>";
             echo "<input type='hidden' name='termin_index' value='$index'>";
-            echo "<input type='submit' name='submit' value='Buchen' onclick='return validateSelection($index)'>";
+            echo "<input type='submit' name='submit' value='Buchen'>";
             echo "</form>";
         } else {
             // Wenn ein Spielername vorhanden ist, einfach den Termin in der Tabelle ausgeben
             echo "$spielerName";
         }
         echo "</td>";
-        
+
         // Spalte für "Termin freigeben"
         echo "<td>";
         if (!empty($spielerName)) {
             echo "<form method='post'>";
-            echo "<input type='checkbox' name='release_check[$index]' value='1' id='releaseCheck-$index'>";
             echo "<input type='hidden' name='termin_index' value='$index'>";
-            echo "<input type='submit' name='release' value='Freigeben' id='releaseButton-$index' disabled>";
+            echo "<input type='submit' name='release' value='Freigeben'>";
             echo "</form>";
-            echo "<script>
-                document.getElementById('releaseCheck-$index').addEventListener('change', function() {
-                    document.getElementById('releaseButton-$index').disabled = !this.checked;
-                });
-            </script>";
         }
         echo "</td>";
-        
+
         echo "</tr>";
     }
 } else {
     // Wenn keine Termine vorhanden sind
     echo "<tr><td colspan='3'>Keine Termine vorhanden.</td></tr>";
 }
+?>
+</table>
 
-echo "</table>";
-echo "<p class='hinweis'>Um für einen Spieltag die Trikotwäsche zu übernehmen, in der Tabelle den gewünschten Termin auswählen und mit einem Klick auf Buchen bestätigen. Sollte ein bereits gebuchter Termin nicht übernommen werden können, kann er über die Funktion 'Termin freigeben' zur erneuten Buchung für eine andere Familie verfügbar gemacht werden.</p>";
+<p class='hinweis'>Um für einen Spieltag die Trikotwäsche zu übernehmen, in der Tabelle den gewünschten Termin auswählen und mit einem Klick auf Buchen bestätigen. Sollte ein bereits gebuchter Termin nicht übernommen werden können, kann er über die Funktion 'Termin freigeben' zur erneuten Buchung für eine andere Familie verfügbar gemacht werden.</p>
+
+<br>
 
 echo "<br>";
 
@@ -169,3 +168,4 @@ echo "<p><a href='termine.php'>Terminplaner verwalten</a></p>";
 
 </body>
 </html>
+
