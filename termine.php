1f430b9587a86f66b89f29b9b42798e5dde378f9
flschr <rene@gaehn.org>
2024-02-12 20:34:18 +0100

    test

--- termine.php
+++ termine.php
@@ -1,7 +1,7 @@
 <?php
 include 'functions.php';
 
-// Fehlermeldungen einschalten
+// Fehlermeldungen einschalten OK
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
