b80222848e7fbe41160868e92850d7b9f4714699
flschr <rene@gaehn.org>
2024-02-12 20:41:20 +0100

    Update termine.php

--- termine.php
+++ termine.php
@@ -73,9 +73,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["new_date"])) {
                     </td>
                     <td>
                         <form id="cancel_form_<?php echo $appointment;?>" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="display:inline;">
-                            <input type="checkbox" name="cancel_checkbox_<?php echo $appointment;?>" value="<?php echo $appointment;?>" onchange="toggleCancelButton('<?php echo $appointment;?>')">
-                            <input type="hidden" name="cancel_date" value="<?php echo $appointment;?>">
-                            <input type="submit" value="Termin absagen" id="cancel_button_<?php echo $appointment;?>" name="cancel_button_<?php echo $appointment;?>" disabled>
+							<input type="checkbox" id="cancel_checkbox_<?php echo $appointment;?>" value="<?php echo $appointment;?>" onchange="toggleCancelButton('<?php echo $appointment;?>')">
+							<input type="hidden" name="cancel_date" value="<?php echo $appointment;?>">
+							<input type="submit" value="Termin absagen" id="cancel_button_<?php echo $appointment;?>" name="cancel_button_<?php echo $appointment;?>" disabled>
                         </form>
                     </td>
                 </tr>
@@ -84,14 +84,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["new_date"])) {
     </table>
 
 <script>
-    $( function() {
-        $( "#datepicker" ).datepicker({dateFormat: 'dd.mm.yy'});
+    $(function() {
+        $("#datepicker").datepicker({dateFormat: 'dd.mm.yy'});
     });
 
     function toggleCancelButton(appointment) {
-        var checkbox = $('#cancel_form_' + appointment + ' input[type="checkbox"]');
+        var checkbox = $('#cancel_checkbox_' + appointment);
         var button = $('#cancel_button_' + appointment);
-        if (checkbox.is(':checked')) {
+        if (checkbox.prop('checked')) {
             button.prop('disabled', false);
         } else {
             button.prop('disabled', true);
