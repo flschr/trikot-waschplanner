f603472fb859f901786029772d07314819e78e5d
flschr <rene@gaehn.org>
2024-02-17 00:19:56 +0100

    Update termine.php

--- termine.php
+++ termine.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     // Speichern der aktuellen URL
     $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI'];
