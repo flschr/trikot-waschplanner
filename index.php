d6730fedb1f2caaabfa666c416d5843d2135d6a4
flschr <rene@gaehn.org>
2024-02-16 21:32:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://kit.fontawesome.com/f96e40973d.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
